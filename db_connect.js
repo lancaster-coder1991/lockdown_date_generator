2f223cbe52d2b900b3fa0bf222f3409932725bdf
lancaster-coder1991 <grjscott1991@hotmail.com>
2021-01-06 10:27:04 +0000

    test db connection

--- db_connect.js
+++ db_connect.js
@@ -1,5 +1,7 @@
 const { Pool } = require("pg");
 
+//
+
 const pool = new Pool({
   user: "george",
   host: "localhost",
@@ -9,20 +11,3 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-
-// const test =
-//   connection === heroku
-//     ? new Pool({
-//         user: "george",
-//         host: "localhost",
-//         database: "lockdown_dates",
-//         password: "georgescott123",
-//         port: 5432,
-//       })
-//     : new Pool({
-//         user: "george",
-//         host: "localhost",
-//         database: "lockdown_dates_test",
-//         password: "georgescott123",
-//         port: 5432,
-//       });
