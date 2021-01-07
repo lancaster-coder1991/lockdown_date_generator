ea112a7f735bce299bb170905a67f6d5101bc3a8
lancaster-coder1991 <grjscott1991@hotmail.com>
2021-01-07 10:51:24 +0000

    changed db_connect file to match heroku settings

--- db_connect.js
+++ db_connect.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "george",
-  host: "localhost",
-  database: "lockdown_dates_test",
-  password: "georgescott123",
+  user: "zevmkvyudjpark",
+  host: "ec2-52-214-38-135.eu-west-1.compute.amazonaws.com",
+  database: "d22e7dqidakad3",
+  password: "0f3a725645461813e70a0a8c7ba234cc623e791af45b97b4a1ba34c71070a55a",
   port: 5432,
 });
 
