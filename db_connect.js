f877b1614ace9c5118bb10457f27dea806dec431
lancaster-coder1991 <grjscott1991@hotmail.com>
2021-01-08 12:44:28 +0000

    heroku db connection works. Now find a way to mask local connection string in separate module

--- db_connect.js
+++ db_connect.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 const { connectionString } = process.env;
-const ENV = process.env.CONNECTION_STRING ? "development" : "test";
+const ENV = process.env.DATABASE_URL ? "development" : "test";
 
 const pool =
   ENV === "development"
@@ -12,12 +12,3 @@ const pool =
 console.log(pool);
 
 module.exports = pool;
-
-/*  ? new Pool({
-        user: "zevmkvyudjpark",
-        host: "ec2-52-214-38-135.eu-west-1.compute.amazonaws.com",
-        database: "d22e7dqidakad3",
-        password:
-          "0f3a725645461813e70a0a8c7ba234cc623e791af45b97b4a1ba34c71070a55a",
-        port: 5432,
-      }) */
