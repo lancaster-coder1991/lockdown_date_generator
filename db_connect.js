592b581cde236c2933cea171a0be91eb759ed47b
lancaster-coder1991 <grjscott1991@hotmail.com>
2021-01-08 12:14:04 +0000

    test heroku db connection

--- db_connect.js
+++ db_connect.js
@@ -4,16 +4,18 @@ const ENV = process.env.CONNECTION_STRING ? "development" : "test";
 
 const pool =
   ENV === "development"
-    ? new Pool({
+    ? new Pool("psql://lockdown-date-server.herokuapp.com/lockdown_dates")
+    : new Pool({ connectionString });
+
+console.log({ connectionString });
+
+module.exports = pool;
+
+/*  ? new Pool({
         user: "zevmkvyudjpark",
         host: "ec2-52-214-38-135.eu-west-1.compute.amazonaws.com",
         database: "d22e7dqidakad3",
         password:
           "0f3a725645461813e70a0a8c7ba234cc623e791af45b97b4a1ba34c71070a55a",
         port: 5432,
-      })
-    : new Pool({ connectionString });
-
-console.log({ connectionString });
-
-module.exports = pool;
+      }) */
