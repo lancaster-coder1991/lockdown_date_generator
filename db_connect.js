5c003fce651af0f8f58301939eea18f2be2f2fb8
lancaster-coder1991 <grjscott1991@hotmail.com>
2021-01-08 12:40:07 +0000

    test connection with DATABASE_URL set as connectionString

--- db_connect.js
+++ db_connect.js
@@ -5,12 +5,11 @@ const ENV = process.env.CONNECTION_STRING ? "development" : "test";
 const pool =
   ENV === "development"
     ? new Pool({
-        connectionString:
-          "psql://zevmkvyudjpark:0f3a725645461813e70a0a8c7ba234cc623e791af45b97b4a1ba34c71070a55a@lockdown-date-server.herokuapp.com:5432/lockdown_dates",
+        connectionString: process.env.DATABASE_URL,
       })
     : new Pool({ connectionString });
 
-console.log("hi");
+console.log(pool);
 
 module.exports = pool;
 
