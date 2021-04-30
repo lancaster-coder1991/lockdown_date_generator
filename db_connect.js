dd40f0cc5b3a0a911f7a1047a30cb7deed103534
George Scott <george.scott@bjss.com>
2021-04-30 12:44:59 +0100

    amended db_connect file with hardcoded connection string for dev environment

--- db_connect.js
+++ db_connect.js
@@ -6,7 +6,12 @@ const ENV = process.env.DATABASE_URL ? "development" : "test";
 const pool =
   ENV === "development"
     ? new Pool({
-        connectionString: process.env.DATABASE_URL,
+        // connectionString: process.env.DATABASE_URL,
+        connectionString:
+          "postgres://zevmkvyudjpark:0f3a725645461813e70a0a8c7ba234cc623e791af45b97b4a1ba34c71070a55a@ec2-52-214-38-135.eu-west-1.compute.amazonaws.com:5432/d22e7dqidakad3",
+        ssl: {
+          rejectUnauthorized: false,
+        },
       })
     : new Pool({
         host: "localhost",
